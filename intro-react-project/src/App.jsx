01e0dff8349d4c2d7788cc4859b76c1d76b31b1a
Olga <olga.gonchar@metropolia.fi>
2026-08-30 11:48:41 +0300

    week2

---
+++ intro-react-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from './Hello';
+import Bye from './Bye';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
